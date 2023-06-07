9381fcffa8df689011c0281cab7c796da92b3ed0
AdrienBlanco <adrienblanco89@gmail.com>
2023-06-07 10:44:13 +0200

    Ajout de la page login, fonction de connexion du user et enregistrement du token de connexion

--- FrontEnd/assets/scripts/gallery.js
+++ FrontEnd/assets/scripts/gallery.js
@@ -4,18 +4,19 @@ const works = await responseWorks.json();
     
 //Génération des travaux présents sur l'API
 generateWorks(works);
-async function generateWorks(works){
+async function generateWorks(works){    
     for (let i = 0; i < works.length; i++) {
         const work = works[i];
         //Récupération emplacement du DOM pour la création des éléments
         const divGallery = document.querySelector('.gallery');
         //Création des balises figure pour chaque travaux
         const workElement = document.createElement('figure');
-        //Création des balises à l'intérieur de chaque travaux, ainsi que leur contenu
+        //Création des balises à l'intérieur de chaque travaux et ajout du contenu
         const imageElement = document.createElement('img');
         imageElement.src = work.imageUrl;
         imageElement.alt = work.title;
         const captionElement = document.createElement('figcaption');
+        captionElement.innerText = work.title;
         //Rattachement des balises aux parents
         divGallery.appendChild(workElement);
         workElement.appendChild(imageElement);
@@ -37,20 +38,18 @@ async function generateCategories(categories){
         //Création des balises li pour chaque boutons
         const filterElement = document.createElement('li');
         filterElement.innerText = category.name;
-        filterElement.classList.add(`${category.id}`);
+        filterElement.dataset.categoryId = category.id;
         //Rattachement des balises aux parents
         filters.appendChild(filterElement);
     }
 };
 
+//Fonction pour vider la gallerie
+function clearWorks(){document.querySelector(".gallery").innerHTML = ""};
+
 //Filtrage par catégorie
 filterByCategory();
 function filterByCategory() {
-
-    //Fonction pour vider la gallerie
-    function clearWorks() {
-        document.querySelector(".gallery").innerHTML = "";
-    };
 
     //Récupération de la liste de boutons sous forme de tableau
     const filterButtons = document.querySelectorAll('.filters li');
@@ -59,20 +58,25 @@ function filterByCategory() {
     filterButtons.forEach(button => {
         //ajout d'un listener pour chaque "button"
         button.addEventListener("click", function () {
-            //Supprimer l'id sur chaque "button" de "filterButtons"
-            filterButtons.forEach(button => {button.removeAttribute('id')});
-            //Récupération des Id des catégories via le nom de la class de chaque "button"
-            let filterByClass = button.className;
-            if(filterByClass === 'no-filter'){
-                button.setAttribute('id','filter-selected');
-                clearWorks();
+            //Suppression de la classe "filter-selected" sur chaque "button" de "filterButtons"
+            filterButtons.forEach(button => {button.classList.remove('filter-selected')});
+            //Récupération de l'Id du button sélectionné grâce à la valeur de l'attribut data-category-id
+            let selectedCategoryId = button.dataset.categoryId;
+            //Ajout de la classe "filter-selected" sur le button sélectionné et suppression de la gallerie de travaux
+            button.classList.add('filter-selected'); 
+            clearWorks();
+            if(button.classList.contains('no-filter')) { //Si le button sélectionné contient la classe "no-filter", générer tous les travaux
                 generateWorks(works);
-            } else {
-                button.setAttribute('id','filter-selected');
-                let filteredWorks = works.filter(work => work.categoryId == filterByClass);
-                clearWorks();
+            } else { //Sinon générer uniquement les travaux filtrés par Id 
+                let filteredWorks = works.filter(work => work.categoryId == selectedCategoryId);
                 generateWorks(filteredWorks);
             }
         });
     });
 };
+
+//Gestion du status User Login / Logout dans le local storage
+const connected = window.localStorage.getItem('validUser');
+console.log(connected);
+
+const logout = window.localStorage.removeItem('validUser');
---
+++ FrontEnd/assets/scripts/login.js
@@ -0,0 +1,31 @@
+//Formulaire de connexion de l'utilisateur
+loginUser();
+function loginUser() {
+    //Ajout d'un eventListener sur le formulaire de connexion
+    document.querySelector('#login form').addEventListener('submit', async function(event) {
+        //Annulation du comportement par défaut
+        event.preventDefault();
+        //Récupération de la valeur saisie dans les champs email et password
+        const loginValue = {
+            email: event.target.querySelector("[name=email]").value,
+            password: event.target.querySelector("[name=password]").value
+        }
+        //Réécriture de la valeur des champs email et password au format Json
+        const loginValueJson = JSON.stringify(loginValue);
+        //Options de la requète fetch 
+        const requestOptions = {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: loginValueJson
+        };
+        //requête API pour authentification du User
+        const responseLogin = fetch('http://localhost:5678/api/users/login', requestOptions);
+        if ((await responseLogin).ok) { //Si l'authentication de l'utilisateur est ok (status 200)
+            window.localStorage.setItem('validUser', loginValueJson); //Enregistrement des informations de connection au format Json dans le localStorage
+            window.location.assign('../../index.html'); //Redirection vers la page principale du site
+        } else {
+            const error = document.querySelector('#login .login-error'); // Affichage d'un message d'erreur en cas de connection non autorisée
+            error.innerText = "Erreur dans l’identifiant ou le mot de passe";
+        };
+    });
+};
